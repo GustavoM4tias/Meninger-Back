bc3dfa2f8e1894b731f07af473bc54d9437c79fc
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-10-15 12:46:06 +0000

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +9,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: process.env.NODE_ENV === 'production' ? '/nome-do-repositorio/' : '/', // Adicione esta linha
+});
