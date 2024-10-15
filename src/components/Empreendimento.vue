1d691ecd1847c71a9863496adfc1e6d322962996
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-10-15 21:00:20 +0000

    Create Nav

---
+++ src/components/Empreendimento.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="shadow-xl rounded-xl hover:shadow-2xl duration-300" @click="handleClick">
+        <a href="#" class="relative block rounded-xl overflow-hidden duration-300 transform hover:scale-105 h-full">
+            <div class="h-full w-full overflow-hidden">
+                <img :src="produto.foto" alt="Imagem do produto" class="h-full w-full object-cover" />
+            </div>
+
+            <div class="absolute inset-0 rounded-xl bg-gradient-to-t from-gray-900 to-transparent opacity-75"></div>
+
+            <div class="absolute inset-0 flex flex-col justify-end text-white p-5">
+                <span class="text-gray-300 uppercase text-sm">{{ produto.cidade }}</span>
+                <p class="text-2xl font-bold truncate">{{ produto.nome }}</p>
+                <p class="text-3xl font-semibold">R$ {{ produto.preco.preco_m2 }} <span
+                        class="text-lg text-gray-300">M²</span></p> <!-- .toFixed(2).replace('.', ',') -->
+            </div>
+
+
+
+            <div class="absolute inset-0 flex justify-between text-white p-5">
+                <div class="">
+                    <button v-if="temCampanhaAtiva" class="bg-green-500 hover:bg-green-600 duration-100 text-white px-3 py-1 rounded-xl text-sm text-gray-200">
+                        Campanha Ativa
+                    </button>
+                    <button v-else-if="semCampanhaAtiva" class="bg-red-500 hover:bg-red-600 duration-100 text-white px-3 py-1 rounded-xl text-sm text-gray-200">
+                        Sem Campanha Ativa
+                    </button>
+                </div>
+                <div class="">
+                    <a :href="produto.link_site1" target="_blank" class="text-2xl font-bold truncate mr-3">
+                        <i class="fa-solid hover:text-gray-200 duration-200 fa-share-from-square"
+                            data-tippy-content="Site"></i>
+                    </a>
+                    <a :href="produto.link_site2" target="_blank" class="text-2xl font-bold truncate">
+                        <i class="fa-solid hover:text-gray-200 duration-200 fa-chart-column"
+                            data-tippy-content="CV CRM"></i>
+                    </a>
+                </div>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, withModifiers, computed } from 'vue';
+
+const props = defineProps({
+    produto: {
+        type: Object,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['click']);
+
+// Função para verificar se há campanhas ativas
+const temCampanhaAtiva = computed(() => {
+    return props.produto.campanhas.some(campanha => campanha.status === true);
+});
+
+// Função para verificar se todas as campanhas estão inativas
+const semCampanhaAtiva = computed(() => {
+    return props.produto.campanhas.length > 0 && !temCampanhaAtiva.value;
+});
+
+const handleClick = withModifiers(() => {
+    emit('click', props.produto);
+}, ['stop']);
+</script>
+
+<style scoped>
+/* Se precisar de ajustes adicionais, você pode adicionar aqui */
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class=" bg-blue-400 p-2 m-4 rounded-lg group">
+        <ul class="text-white text-lg">
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção1</span></li>
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção2</span></li>
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção3</span></li>
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção4</span></li>
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção5</span></li>
+            <li class="bg-blue-600 hover:bg-blue-700 duration-200 my-2 px-3 py-1 rounded-md"><i class="fas fa-chevron-right"></i> <span class="texto opacity-0 duration-300 group-hover:opacity-100">Opção6</span></li>
+        </ul>
+    </nav>
+</template>
