44695060134ee124f30d75338fa523a791f40288
GustavoM4tias <faculdogu@gmail.com>
2024-10-19 03:08:49 -0300

    Responsividade Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="bg-gray-50 cs-font" v-if="user">
-    <div id="wrapper" class="bg-blue-900 grid h-screen grid-cols-1 xl:grid-cols-2 xl:h-screen relative">
+    <div id="wrapper" class="bg-blue-900 h-screen w-screen grid grid-cols-1 xl:grid-cols-2 relative">
 
-      <div id="col-1" class="px-12 pt-32 pb-48 md:px-32 xl:py-64 xl:px-32 z-50">
-        <p class="text-blue-500  font-extrabold text-6xl md:text-8xl">
+      <div id="col-1" class="m-auto z-50">
+        <p class="text-blue-500 font-extrabold text-6xl md:text-8xl" style="line-height: .85">
           Bem <br />
-          Vindo <br />
+          Vindo! <br />
           {{ user.cargo }} <br />
-          {{ user.nome }}
+          {{ user.nome }}.
         </p>
-        <p class="text-white text-normal text-3xl md:text-4xl pt-3 md:pt-6 font-medium">
-          Cidade: {{ user.cidade }}
+        <p class="text-white text-3xl md:text-4xl font-medium">
+          {{ user.cidade }}
         </p>
       </div>
 
-      <img src="/traçado.png" class="absolute left-1/2 md:left-1/3 transform -translate-x-1/2 top-10 w-11/12 sm:w-8/12 opacity-50 z-1">
 
-      <div id="col-2" class="bg-gray-50 px-3 md:px-20 flex flex-col justify-start md:justify-center z-50 h-full">
+      <img src="/traçado.png"
+        class="absolute left-1/2 md:left-1/3 transform -translate-x-1/2 top-10 w-11/12 sm:w-8/12 opacity-50 z-1">
+
+      <div id="col-2" class="bg-gray-50 px-3 md:px-20 py-8 flex flex-col justify-end md:justify-center z-50 h-full">
 
         <RouterLink
           class="rounded-md flex border pl-6 py-4 -mt-52 md:-mt-6 bg-gray-100 hover:bg-gray-200 duration-100 xl:-ml-24 pl-6 xl:rounded-xl shadow-md cursor-pointer"
@@ -89,12 +91,12 @@ import { useRouter } from 'vue-router';
 
 const userStore = useUserStore();
 const router = useRouter();
-const user = ref(null); // Inicializa como null
+const user = ref(null); 
 
 const loadUser = async () => {
-  const token = localStorage.getItem('token'); // Obter o token do localStorage
+  const token = localStorage.getItem('token'); 
   if (!token) {
-    router.push('/login'); // Redireciona para o login se não houver token
+    router.push('/login'); 
     return;
   }
 
@@ -103,7 +105,7 @@ const loadUser = async () => {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}` // Enviar o token no cabeçalho
+        'Authorization': `Bearer ${token}`
       },
     });
 
@@ -112,22 +114,22 @@ const loadUser = async () => {
     }
 
     const usuario = await response.json();
-    userStore.setUser(usuario); // Armazenar as informações no store
-    user.value = usuario; // Atualiza a referência local
+    userStore.setUser(usuario); 
+    user.value = usuario; 
     // Mostra dados usuario    console.log(user)
   } catch (error) {
     console.error('Erro ao carregar usuário:', error);
-    router.push('/login'); // Redireciona para o login em caso de erro
+    router.push('/login'); 
   }
 };
 
 onMounted(() => {
-  loadUser(); // Chama a função ao montar o componente
+  loadUser(); 
 });
 
 const logout = () => {
-  userStore.clearUser(); // Limpa o usuário do store
-  localStorage.removeItem('token'); // Remove o token do localStorage
-  router.push('/login'); // Redireciona para a página de login
+  userStore.clearUser(); 
+  localStorage.removeItem('token'); 
+  router.push('/login'); 
 };
 </script>
