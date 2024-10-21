f42d59d584889faba6f30722fc237894c0b9bb7b
GustavoM4tias <faculdogu@gmail.com>
2024-10-21 17:07:13 -0300

    Card Blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,39 +1,8 @@
-<template>
-    <div class="container mx-auto my-10">
-        <h1 class="text-3xl font-bold mb-5">Blog</h1>
-
-        <div class="mb-10">
-            <h2 class="text-2xl font-semibold mb-3">Posts Recentes</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <PostCard v-for="evento in eventosRecentes" :key="evento.id" :evento="evento"
-                    @abrir-modal="abrirModal(evento)" />
-            </div>
-        </div>
-
-        <div>
-            <h2 class="text-2xl font-semibold mb-3">Eventos em Andamento</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <PostCard v-for="evento in eventosEmAndamento" :key="evento.id" :evento="evento"
-                    @abrir-modal="abrirModal(evento)" />
-            </div>
-        </div>
-
-        <div>
-            <h2 class="text-2xl font-semibold mb-3">Eventos Finalizados</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <PostCard v-for="evento in eventosFinalizados" :key="evento.id" :evento="evento"
-                    @abrir-modal="abrirModal(evento)" />
-            </div>
-        </div>
-
-        <Modal v-if="eventoModal" @fechar-modal="eventoModal = null" :evento="eventoModal" />
-    </div>
-</template>
-
 <script setup>
 import { ref, onMounted, computed } from 'vue';
-import PostCard from '../components/Eventos/PostCard.vue'; // Componente do card
+import Card from '../components/Eventos/Card.vue'; // Componente do card
 import Modal from '../components/Eventos/Modal.vue'; // Componente do modal
+import Nav from '../components/Eventos/Nav.vue'; // Componente do modal
 
 const eventos = ref([]);
 const eventoModal = ref(null);
@@ -68,6 +37,43 @@ const eventosRecentes = computed(() => {
 
 onMounted(fetchEventos);
 </script>
+
+<template>
+    <div class="container mx-auto my-10">
+        <h1 class="text-4xl text-center font-bold mb-5">Eventos Marketing</h1>
+
+        <Nav class="fixed top-80 sm:top-20 left-10" />
+
+        <div class="mb-10">
+            <h2 class="text-2xl font-semibold mb-3">Eventos em Andamento</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <Card v-for="evento in eventosEmAndamento" :key="evento.id" :evento="evento"
+                    @abrir-modal="abrirModal(evento)" />
+            </div>
+        </div>
+
+        <div class="mb-10">
+            <h2 class="text-2xl font-semibold mb-3">Posts Recentes</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <Card v-for="evento in eventosRecentes" :key="evento.id" :evento="evento"
+                    @abrir-modal="abrirModal(evento)" />
+            </div>
+        </div>
+
+
+        <div class="pb-20">
+            <h2 class="text-2xl font-semibold mb-3">Eventos Finalizados</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <Card v-for="evento in eventosFinalizados" :key="evento.id" :evento="evento"
+                    @abrir-modal="abrirModal(evento)" />
+            </div>
+        </div>
+
+        <Modal v-if="eventoModal" @fechar-modal="eventoModal = null" :evento="eventoModal" />
+    </div>
+</template>
+
+
 
 <style scoped>
 /* Estilos adicionais, se necessário */
