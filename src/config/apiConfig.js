7ccccb83dd13d5d6231135ac24a4a8e5577c8a92
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-10-25 22:08:25 -0300

    Enviando alteracoes para alteracao

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,4 @@
-const apiUrl = import.meta.env.VITE_API_URL || 'https://meninger-back.vercel.app/api/';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/';
 
 export default {
   apiUrl,
