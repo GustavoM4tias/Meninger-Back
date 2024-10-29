3e8decbd987ed5880be310df2dfda9b15dd6e294
GustavoM4tias <faculdogu@gmail.com>
2024-10-29 10:16:44 -0300

    Get User Infos

--- models/userModel.js
+++ models/userModel.js
@@ -1,22 +1,25 @@
 // api/models/userModel.js
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 
 const User = {
-  // Registrar um novo usuário
   register: async (db, username, password, email) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    // console.log('Senha hasheada:', hashedPassword);  // Verifique se está correto
     const sql = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
     const [result] = await db.execute(sql, [username, hashedPassword, email]);
     return result;
   },
 
-  // Encontrar usuário pelo nome de usuário
   findByUsername: async (db, username) => {
     const sql = 'SELECT * FROM users WHERE username = ?';
     const [rows] = await db.execute(sql, [username]);
     return rows[0];
   },
+
+  findById: async (db, id) => {
+    const sql = 'SELECT * FROM users WHERE id = ?';
+    const [rows] = await db.execute(sql, [id]);
+    return rows[0];
+  },
 };
 
-module.exports = User;
+export default User;
--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,11 +1,17 @@
 // api/utils/responseHandler.js
 const responseHandler = {
-    success: (res, data) => {
-      res.json({ success: true, data });
-    },
-    error: (res, error) => {
-      res.status(500).json({ success: false, error });
-    },
-  };
-  
-  module.exports = responseHandler;
+  success: (res, data) => {
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  },
+  error: (res, error) => {
+    res.status(500).json({
+      success: false,
+      error: error.message || 'O servidor encontrou uma situação inesperada.',
+    });
+  },
+};
+
+export default responseHandler;
