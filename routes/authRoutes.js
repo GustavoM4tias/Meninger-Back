808f6f29ce7da9398fb140c6e39290fb8a06966a
GustavoM4tias <faculdogu@gmail.com>
2024-11-08 17:56:03 -0300

    Edit Account

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 // api/routes/authRoutes.js
 import express from 'express';
-import { registerUser, loginUser, getUserInfo } from '../controllers/authController.js';
+import { registerUser, loginUser, getUserInfo, updateUser } from '../controllers/authController.js';
 import authenticate from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/user', authenticate, getUserInfo);
+router.put('/user', authenticate, updateUser); // Nova rota para atualização
 
 export default router;
