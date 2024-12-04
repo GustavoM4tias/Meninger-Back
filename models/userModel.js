606725a11824f973dfdd8278b48a43d37a78c451
GustavoM4tias <faculdogu@gmail.com>
2024-12-04 20:32:25 -0300

    User status added

--- models/userModel.js
+++ models/userModel.js
@@ -4,8 +4,8 @@ import bcrypt from 'bcryptjs';
 const User = {
   register: async (db, username, password, email, position, city) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const sql = 'INSERT INTO users (username, password, email, position, city) VALUES (?, ?, ?, ?, ?)';
-    const [result] = await db.execute(sql, [username, hashedPassword, email, position, city]);
+    const sql = 'INSERT INTO users (username, password, email, position, city, status) VALUES (?, ?, ?, ?, ?)';
+    const [result] = await db.execute(sql, [username, hashedPassword, email, position, city, true]);
     return result;
   },
 
@@ -28,15 +28,15 @@ const User = {
   },
 
   // Método para atualizar usuário por ID
-  updateById: async (db, id, { username, email, position, city }) => {
-    const sql = 'UPDATE users SET username = ?, email = ?, position = ?, city = ? WHERE id = ?';
-    const [result] = await db.execute(sql, [username, email, position, city, id]);
+  updateById: async (db, id, { username, email, position, city, status}) => {
+    const sql = 'UPDATE users SET username = ?, email = ?, position = ?, city = ?, status = ? WHERE id = ?';
+    const [result] = await db.execute(sql, [username, email, position, city, status, id]);
     return result.affectedRows > 0;
   },
   
   // Método para buscar todos os usuários
   findAll: async (db) => {
-    const sql = 'SELECT id, username, email, position, city FROM users'; // Selecione apenas os campos necessários
+    const sql = 'SELECT id, username, email, position, city, status FROM users'; // Selecione apenas os campos necessários
     const [rows] = await db.execute(sql);
     return rows;
   },
