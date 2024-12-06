b37e65f1e58f40d21bf545f611601ed902a1832a
GustavoM4tias <faculdogu@gmail.com>
2024-12-06 18:46:17 -0300

    Adjusts Building and Events

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,15 +1,17 @@
 // api/utils/responseHandler.js
 const responseHandler = {
   success: (res, data) => {
+    console.log('Success:', data); // Log adicional para depuração
     res.status(200).json({
       success: true,
       data,
     });
   },
   error: (res, error) => {
+    console.error('Error:', error); // Log para identificar o problema
     res.status(500).json({
       success: false,
-      error: error.message || 'O servidor encontrou uma situação inesperada.',
+      error: typeof error === 'string' ? error : error.message || 'Erro inesperado no servidor.',
     });
   },
 };
