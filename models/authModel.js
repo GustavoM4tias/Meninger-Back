cae670a1f83e89946e7f30637b71a1f87ea5b1c3
GustavoM4tias <faculdogu@gmail.com>
2024-12-09 17:48:37 -0300

    Email adjust and birthday added in users

--- models/authModel.js
+++ models/authModel.js
@@ -2,10 +2,11 @@
 import bcrypt from 'bcryptjs';
 
 const User = {
-  register: async (db, username, password, email, position, city) => {
+  // Método para registrar novo usuário, incluindo data de nascimento
+  register: async (db, username, password, email, position, city, birth_date) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const sql = 'INSERT INTO users (username, password, email, position, city, status) VALUES (?, ?, ?, ?, ?)';
-    const [result] = await db.execute(sql, [username, hashedPassword, email, position, city, true]);
+    const sql = 'INSERT INTO users (username, password, email, position, city, birth_date, status) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const [result] = await db.execute(sql, [username, hashedPassword, email, position, city, birth_date, true]);
     return result;
   },
 
@@ -27,13 +28,13 @@ const User = {
     return rows[0];
   },
 
-  // Método para atualizar usuário por ID
-  updateById: async (db, id, { username, email, position, city, status}) => {
-    const sql = 'UPDATE users SET username = ?, email = ?, position = ?, city = ?, status = ? WHERE id = ?';
-    const [result] = await db.execute(sql, [username, email, position, city, status, id]);
+  // Método para atualizar usuário por ID, incluindo data de nascimento
+  updateById: async (db, id, { username, email, position, city, status, birth_date }) => {
+    const sql = 'UPDATE users SET username = ?, email = ?, position = ?, city = ?, status = ?, birth_date = ? WHERE id = ?';
+    const [result] = await db.execute(sql, [username, email, position, city, status, birth_date, id]);
     return result.affectedRows > 0;
   },
-  
+
   // Método para buscar todos os usuários
   findAll: async (db) => {
     const sql = 'SELECT id, username, email, position, city, status FROM users'; // Selecione apenas os campos necessários
