05783fe7295e7463dc801105ccc8b42ab1fd93f8
GustavoM4tias <faculdogu@gmail.com>
2025-01-30 15:31:00 -0300

    Last update after changing

--- routes/externalApiRoutes.js
+++ routes/externalApiRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { fetchReservations } from '../controllers/externalApiController.js';
+import { fetchReservations, fetchDistracts } from '../controllers/externalApiController.js';
 
 const router = express.Router();
 
 // Endpoint para buscar reservas
 router.get('/reservas', fetchReservations);
 
+// New route for distracts
+router.get('/distratos', fetchDistracts);
+
 export default router;
+
