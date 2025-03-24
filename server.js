6382ba148e889facd8607fe361c45468c92cf939
GustavoM4tias <faculdogu@gmail.com>
2025-03-24 09:47:17 -0300

    CORS POLICE ADJUST 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import db from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import eventRoutes from './routes/eventRoutes.js'; 
+import eventRoutes from './routes/eventRoutes.js';
 import favoriteRoutes from './routes/favoriteRoutes.js';
 import externalApiRoutes from './routes/externalApiRoutes.js';
 // import plannerRoutes from './routes/plannerRoutes.js';
@@ -16,18 +16,23 @@ const app = express();
 app.use(express.json());
 
 // Usar CORS
-// app.use(cors({
-//   origin: ['http://localhost:5173', 'https://meninger.vercel.app'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true,
-// }));
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://meninger.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+// app.use(cors());
 
 // Middleware para adicionar `req.db` em cada requisição
 app.use((req, res, next) => {
   req.db = db;
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
 });
+
 
 // Rotas
 app.use('/api/auth', authRoutes);
