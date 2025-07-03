cc835e7645b4b588b92765b18198ceb5f4ef6e28
GustavoM4tias <faculdogu@gmail.com>
2025-07-03 10:44:10 -0300

    Sequelize adjust

--- config/config.cjs
+++ config/config.cjs
@@ -2,39 +2,39 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username:   process.env.DB_USER,
-    password:   process.env.DB_PASSWORD,
-    database:   process.env.DB_DATABASE,
-    host:       process.env.DB_HOST,
-    port:       process.env.DB_PORT,
-    dialect:    'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     define: {
       underscored: true,
-      timestamps:  true,
+      timestamps: true,
     },
     pool: {
-      max:      10,
-      min:      0,
-      acquire:  30000,
-      idle:     10000,
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     }
-  }, 
+  },
   production: {
-    username:   process.env.DB_USER,     
-    password:   process.env.DB_PASSWORD,
-    database:   process.env.DB_DATABASE,
-    host:       process.env.DB_HOST,
-    port:       process.env.DB_PORT,
-    dialect:    'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     define: {
       underscored: true,
-      timestamps:  true,
+      timestamps: true,
     },
     pool: {
-      max:      10,
-      min:      0,
-      acquire:  30000,
-      idle:     10000,
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     }
   }
 };
