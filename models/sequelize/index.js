f6622a7c9403a2f47804c29ebbcf94c1b8c06ade
GustavoM4tias <faculdogu@gmail.com>
2025-07-17 15:07:02 -0300

    contracts sales cron and data

--- models/sequelize/index.js
+++ models/sequelize/index.js
@@ -7,6 +7,14 @@ import config from '../../config/config.cjs';
 import TokenUsageDefine from './tokenUsage.js';
 import ValidationHistoryDefine from './validationHistory.js';
 
+// Singe imports
+import SalesContractDefine         from './sienge/salesContract.js';
+import SalesContractCustomerDefine from './sienge/salesContractCustomer.js';
+import SalesContractUnitDefine     from './sienge/salesContractUnit.js';
+import PaymentConditionDefine      from './sienge/paymentCondition.js';
+import ContractLinkDefine          from './sienge/contractLink.js';
+
+
 const env = process.env.NODE_ENV || 'development';
 const cfg = config[env];
 const sequelize = new Sequelize(cfg.database, cfg.username, cfg.password, {
@@ -20,6 +28,13 @@ db.Favorite = FavoriteDefine(sequelize, DataTypes);
 db.Event    = EventDefine(sequelize, DataTypes);
 db.TokenUsage = TokenUsageDefine(sequelize, DataTypes);
 db.ValidationHistory = ValidationHistoryDefine(sequelize, DataTypes);
+
+// sienge db 
+db.SalesContract         = SalesContractDefine(sequelize, DataTypes);
+db.SalesContractCustomer = SalesContractCustomerDefine(sequelize, DataTypes);
+db.SalesContractUnit     = SalesContractUnitDefine(sequelize, DataTypes);
+db.PaymentCondition      = PaymentConditionDefine(sequelize, DataTypes);
+db.ContractLink          = ContractLinkDefine(sequelize, DataTypes);
 
 // Se tiver associações, faça-as aqui:
 Object.values(db)
