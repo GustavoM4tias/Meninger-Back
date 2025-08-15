dc243e80ec99ab66aa9329b2121a035dccc07dcc
GustavoM4tias <faculdogu@gmail.com>
2025-08-15 18:19:08 -0300

    leads adapter

---
+++ config/cityMappingLeads.js
@@ -0,0 +1,26 @@
+// src/config/cityMappingLeads.js
+
+export const cvBuildingCityMap = {
+    2: 'Marília', // BOULEVARD PARK & RESORT LTDA
+    9: 'Bauru', // DRUMOND
+    10: 'Marília', // MOND
+    11: 'Marília', // WISHempreendimento": 
+    12: 'Marília', // MAIA STAR CLUB
+    13: 'Marília', // EDIFICIO SOUL
+    14: 'Marília', // LONDON RESIDENCE
+    17: 'Marília', // EDIFÍCIO CONCEPT
+    19: 'Marília', // RESERVA ALTOS DO BOSQUE
+    20: 'Marília', // ADHARA STAR CLUB
+    21: 'Marília', // URBAN ESMERALDAS
+    22: 'Jacarezinho', // PARQUE DOS IPÊS
+    23: 'Marília', // TRES MARIAS
+    24: 'Marília', // JARDIM MARINA FASE 3
+    25: 'Dourados', // JARDIM DOS BURITIS MOD III - IV
+    26: 'Marília', // JARDIM MÔNACO
+    29: 'Marília', // TERRAS DE SÃO PAULO 5
+    30: 'Marília', // MOOV ESMERALDAS
+    31: 'Marília', // JARDIM DAS ROSAS  
+    32: 'Cuiabá', // PARQUE DAS FLORES 
+    33: 'Regente Feijó', // MOACIR MARANGONI 
+    34: 'Sinop', // RESIDENCIAL VERONA 
+}
