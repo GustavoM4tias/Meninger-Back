1e2805cbda1d04ba710f14c9f764916d805f9bc4
GustavoM4tias <gustavodiniz200513@gmail.com>
2026-01-20 16:01:20 -0300

    error ajust

--- models/sequelize/academy/post.js
+++ models/sequelize/academy/post.js
@@ -1,38 +1,37 @@
 export default (sequelize, DataTypes) => {
-    const AcademyPost = sequelize.define(
-        'AcademyPost',
-        {
-            topicId: { type: DataTypes.INTEGER, allowNull: false },
+    const AcademyPost = sequelize.define('AcademyPost', {
+        topicId: { type: DataTypes.INTEGER, allowNull: false },
 
-            // markdown tokens
-            body: { type: DataTypes.TEXT, allowNull: false, defaultValue: '' },
+        // markdown/token body
+        body: { type: DataTypes.TEXT, allowNull: false, defaultValue: '' },
 
-            // payload do TokenEditor (embeds/widgets)
-            payload: { type: DataTypes.JSONB, allowNull: true },
+        // payload TokenEditor (embeds/widgets)
+        payload: { type: DataTypes.JSONB, allowNull: false, defaultValue: {} },
 
-            createdByUserId: { type: DataTypes.INTEGER, allowNull: true },
-            updatedByUserId: { type: DataTypes.INTEGER, allowNull: true },
+        createdByUserId: { type: DataTypes.INTEGER, allowNull: true },
+        updatedByUserId: { type: DataTypes.INTEGER, allowNull: true },
 
-            // ANSWER | COMMENT
-            type: { type: DataTypes.STRING, allowNull: false, defaultValue: 'ANSWER' },
+        // ANSWER | COMMENT
+        type: { type: DataTypes.STRING, allowNull: false, defaultValue: 'ANSWER' },
 
-            upvotes: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-        },
-        {
-            tableName: 'academy_posts',
-            timestamps: true,
-            underscored: true,
-            indexes: [
-                { fields: ['topic_id'] },
-                { fields: ['type'] },
-                { fields: ['created_by_user_id'] },
-                { fields: ['updated_by_user_id'] },
-            ],
-        }
-    );
+        upvotes: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+    }, {
+        tableName: 'academy_posts',
+        timestamps: true,
+        underscored: true,
+        indexes: [
+            { fields: ['topic_id'] },
+            { fields: ['type'] },
+        ],
+    });
 
     AcademyPost.associate = (db) => {
         AcademyPost.belongsTo(db.AcademyTopic, { foreignKey: 'topicId', as: 'topic' });
+
+        if (db.User) {
+            AcademyPost.belongsTo(db.User, { foreignKey: 'createdByUserId', as: 'createdBy' });
+            AcademyPost.belongsTo(db.User, { foreignKey: 'updatedByUserId', as: 'updatedBy' });
+        }
     };
 
     return AcademyPost;
